1ad87bfe8011904b1b0e473d4fddfa4a035c6f0b
StephenT <smturvey@gmail.com>
2015-05-05 11:49:03 +0100

    media query test

--- js/scripts.js
+++ js/scripts.js
@@ -16,22 +16,22 @@ $('a.ou-mobile-menu-toggle').toggleClass('closed open')
 // FILTER NAV
 // this adds/removes a class to the filter nav on click for mobile users
 
-var screencheck = window.matchMedia("(max-width: 768px)");
-if (screencheck.matches) {
+//var screencheck = window.matchMedia("(max-width: 768px)");
+//if (screencheck.matches) {
 
-$(".filters ul.tabs > li > a").click(function(){
+//$(".filters ul.tabs > li > a").click(function(){
 	
-	var t=$(this).parent();
+//	var t=$(this).parent();
 	
-	return t.hasClass("open")?t.removeClass("open"):(
+//	return t.hasClass("open")?t.removeClass("open"):(
 	
-	$(".tabs > li.open").removeClass("open"),t.addClass("open")),!1
+//	$(".tabs > li.open").removeClass("open"),t.addClass("open")),!1
 	
-	return false;
+//	return false;
 	
-});
+//});
 
-}
+//}
 
 
 
